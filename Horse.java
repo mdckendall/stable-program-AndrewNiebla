80a52201b45526e2ae6d43ed955eaff002e65c5b
Andrew Niebla <113561001+AndrewNiebla@users.noreply.github.com>
2022-10-10 19:31:37 +0000

    made horse stable

--- Horse.java
+++ Horse.java
@@ -1,1 +1,18 @@
 //Create the Horse class here.
+public class Horse {
+String hn;
+  double w;
+boolean t;
+
+  
+ public Horse(String hn, double w, boolean t) {
+   this.hn = hn;
+   this.w = w;
+   this.t = t;
+
+}
+  
+}
+ 
+
+  
